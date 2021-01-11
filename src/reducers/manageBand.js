d620038ebd4c17df64ecaf21afd84987524f5256
EvanTheSmith <ProfessionalEmail@Zoho.com>
2021-01-11 13:33:22 -0600

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -2,7 +2,8 @@ export default function manageBand(state = {bands: [],}, action) {
 
   switch (action.type) {
     case 'ADD_BAND':
-      return {...state, bands: [...state.bands, action.band]}
+      // return {...state, bands: [...state.bands, action.band]}
+      return { bands: [...state.bands, action.band] }
     default:
       return state;
   }
